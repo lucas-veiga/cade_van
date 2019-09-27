d909341a3915db548eaf714ef3a6c5501734a5fe
UnDer-7 <mateus7532@gmail.com>
2019-09-26 22:35:18 -0300

    Driver created

---
+++ lib/models/driver.dart
@@ -0,0 +1,12 @@
+import 'package:meta/meta.dart';
+
+import './user.dart';
+
+class Driver {
+  String name;
+  String nickName;
+  String cpf;
+  String email;
+  String phone;
+  String password;
+}
