c798bba88b6f102cb091abbeb572fea93b9d6e55
UnDer-7 <mateus7532@gmail.com>
2019-10-06 15:22:21 -0300

    Google Maps implemented

--- lib/pages/responsible/map_tab.dart
+++ lib/pages/responsible/map_tab.dart
@@ -16,12 +16,17 @@ class MapTab extends StatefulWidget {
 
 class _MapTabState extends State<MapTab> {
   Completer<GoogleMapController> _controller = Completer();
+  BitmapDescriptor _driverIcon;
+  Set<Marker> _markers;
 
   @override
   Widget build(BuildContext context) {
     final User user = Provider.of<UserProvider>(context, listen: false).user;
+    _createMarkerImageFromAsset(context);
+    _addMarker(user);
 
     return GoogleMap(
+      markers: _markers,
       onMapCreated: (final GoogleMapController controller) => _controller.complete(controller),
       initialCameraPosition: CameraPosition(
         target: LatLng(user.userLocation.latitude, user.userLocation.longitude),
@@ -29,4 +34,29 @@ class _MapTabState extends State<MapTab> {
       ),
     );
   }
+
+  void _addMarker(final User user) {
+    _markers = {
+      Marker(
+        markerId: MarkerId('Mototorista'),
+        position: LatLng(user.userLocation.latitude, user.userLocation.longitude),
+        icon: _driverIcon,
+        infoWindow: InfoWindow(
+          title: 'Motorista'
+        ),
+      ),
+    };
+  }
+
+  void _createMarkerImageFromAsset(final BuildContext context) {
+    final ImageConfiguration imageConfiguration =
+      createLocalImageConfiguration(context);
+    BitmapDescriptor.fromAssetImage(
+      imageConfiguration,
+      'assets/images/school_bus.png',
+    ).then(_updateBitMap);
+  }
+
+  void _updateBitMap(final BitmapDescriptor bitmapDescriptor) =>
+    setState(() => _driverIcon = bitmapDescriptor);
 }
