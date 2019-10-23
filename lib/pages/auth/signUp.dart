a596575ef0f14c0981053dc8e3a51001659ef1af
UnDer-7 <mateus7532@gmail.com>
2019-10-23 19:20:27 -0300

    Fix

--- lib/pages/auth/signUp.dart
+++ lib/pages/auth/signUp.dart
@@ -60,31 +60,3 @@ class SignUp extends StatelessWidget {
     );
   }
 }
-
-
-
-/*
-AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back, color: Colors.black),
-            onPressed: () => _pageController.animateToPage(
-              1,
-              duration: Duration(milliseconds: 800),
-              curve: Curves.bounceOut,
-            ),
-          ),
-          backgroundColor: Colors.indigo,
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.face),
-                text: 'Responsável',
-              ),
-              Tab(
-                icon: Icon(Icons.airport_shuttle),
-                text: "Transportador",
-              ),
-            ],
-          ),
-        )
- */
